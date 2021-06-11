ec46b90efca98cb367f9263517538549b8f5e314
paulords95 <paulords95@gmail.com>
2021-06-11 11:33:31 -0300

    login page

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 soap.createClient(process.env.SOAP_ENDPOINT, function (err, client) {
   client.GetUser(
     {
-      user: "user",
-      password: "pass",
+      user: "paulo.silva",
+      password: "1995!Oluap",
       encryption: "0",
       parameters: "",
     },
--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import LoginPage from "./pages/login/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
