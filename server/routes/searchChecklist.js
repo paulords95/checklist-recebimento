49176f3fd7246c9b10908ad63e0409d4fd789621
paulords95 <paulords95@gmail.com>
2021-06-14 16:16:02 -0300

    fixed update route

--- server/routes/searchChecklist.js
+++ server/routes/searchChecklist.js
@@ -50,10 +50,20 @@ router.post("/anwser/", authorization, async (req, res) => {
     const sql =
       "UPDATE USU_T158 SET USU_D5=:form1, USU_D6=:form2, USU_D7=:form3, USU_D8=:form4, USU_D9=:form5 WHERE USU_CODREC = :seqCheck";
 
-    const result = await db(sql, form1, form2, form3, form4, form5, seqCheck);
+    const updateRow = await db(
+      sql,
+      form1,
+      form2,
+      form3,
+      form4,
+      form5,
+      seqCheck
+    );
 
+    console.log(updateRow);
+    const sql1 = "select * from usu_t158 where usu_codrec = :seqCheck1";
+    const result = await db(sql1, seqCheck);
     console.log(result);
-
     res.json(req.body);
   } catch (error) {}
 });
